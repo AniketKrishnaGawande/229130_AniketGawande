89aa6aaed3f775d44b16753f01f5729afa8c7e19
Aniket Gawande <121350093+AniketKrishnaGawande@users.noreply.github.com>
2023-01-02 17:03:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+// import and create express object
 const express = require("express");
 const app = express();
 
-
+// write router api for incoming request
 app.get("/",(req,resp)=>{
-
+// send html file as output
 resp.sendFile("public/Company.html",{root:__dirname});
 
 })
+// start server at port 3000
 app.listen(3000);
+// if server started give msg
 console.log("server started at 3000");
-
-
